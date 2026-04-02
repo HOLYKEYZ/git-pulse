9c2d10dfe830019d3e61af726bfea428bcb545f8
HOLYKEYZ <ayandajoseph390@gmail.com>
2026-04-02 13:46:05 +0100

    update

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -70,12 +70,9 @@ const config: Config = {
           "Menlo",
           "monospace",
         ],
-      customGithubBlue: '#58a6ff',
-      customDarkBg: '#0d1117',
-      customRepoCardHoverBg: '#161b22'
+      },
     },
   },
-};
 };
 
 export default config;

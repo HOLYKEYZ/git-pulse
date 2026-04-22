fc76c951e0e005fbaeb2ebc48b1e8e768a2362ca
HOLYKEYZ <ayandajoseph390@gmail.com>
2026-04-22 22:25:59 +0100

    Production ready release - Purge DOMPurify

--- apps/web/src/components/PostCard.tsx
+++ apps/web/src/components/PostCard.tsx
@@ -237,9 +237,7 @@ const handleReact = async (emoji: string) => {
             }}>
             
             {/* simple pre-processor for #tags and @mentions to turn them into markdown links if not already */}
-            {DOMPurify.sanitize(post.content.replace(/(^|\s)(#[\w-]+)/g, '$1[$2]($2)').replace(/(^|\s)(@[\w-]+)/g, '$1[$2]($2)'), {
-              FORBID_TAGS: ['script', 'style', 'iframe', 'form', 'button', 'input']
-            })}
+            {post.content.replace(/(^|\s)(#[\w-]+)/g, '$1[$2]($2)').replace(/(^|\s)(@[\w-]+)/g, '$1[$2]($2)')}
           </ReactMarkdown>
         </div>
 
